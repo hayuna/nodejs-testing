bf34fa932c619e50256932bd05cb37905aee0d0b
mras2303 <marek.kowalonek@gmail.com>
2019-06-01 16:53:36 +0200

    add test

---
+++ src/chunk.test.js
@@ -0,0 +1,19 @@
+const chunkArray = require('./chunk')
+
+test('chunkArray function exists', () => {
+    expect(chunkArray).toBeDefined()
+})
+
+test('chunk an array of 10values with length of 2', () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const len = 2
+    const chunkedArray = chunkArray(numbers, len)
+    expect(chunkedArray).toStrictEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+})
+
+test('chunk an array of 3 values with length of 1', () => {
+    const numbers = [1, 2, 3]
+    const len = 1
+    const chunkedArray = chunkArray(numbers, len)
+    expect(chunkedArray).toStrictEqual([[1], [2], [3]])
+})
---
+++ src/functions.test.js
@@ -0,0 +1,56 @@
+const functions = require('./functions')
+
+const nameCheck = () => console.log('Checking name ....')
+
+describe('Checking Names', () => {
+    beforeEach(() => nameCheck())
+    test('User is Jeff', () => {
+        const user = 'Jeff'
+        expect(user).toBe('Jeff')
+    })
+    test('User is Karen', () => {
+        const user = 'Karen'
+        expect(user).toBe('Karen')
+    })
+    test('Adds 2 + 2 to equal 4', () => {
+        expect(functions.add(2, 2)).toBe(4)
+    })
+    test('Adds 2 + 2 to NOT equal 5', () => {
+        expect(functions.add(2, 2)).not.toBe(5)
+    })
+    test('Should be null', () => {
+        expect(functions.isNull()).toBeNull()    
+    })
+    test('Should be falsy', () => {
+        expect(functions.checkValue(undefined)).toBeFalsy()    
+    })
+    test('User should be Brad Traversy object', () => {
+        expect(functions.createUser()).toEqual({
+            firstName: 'Brad',
+            lastName: 'Traversy'
+        })
+    })
+    test('Should be under 1600', () => {
+        const load1 = 800;
+        const load2 = 800
+        expect(load1 + load2).toBeLessThanOrEqual(1600)
+    })
+    test('There is no I in team', () => {
+        expect('team').not.toMatch(/I/i)
+    })
+    test('Admin should be in usernames', () => {
+        const usernames = ['john', 'karen', 'admin']
+        expect(usernames).toContain('admin') 
+    })
+    test('User fetched name should be Leanne Graham', () => {
+        expect.assertions(1)
+        return functions.fetchUser().then(data => {
+            expect(data.name).toEqual('Leanne Graham')
+        })
+    })
+    test('User fetched name be Leanne Graham', async () => {
+        expect.assertions(1)
+        const data = await functions.fetchUser()
+        expect(data.name).toEqual('Leanne Graham')
+    })
+})
