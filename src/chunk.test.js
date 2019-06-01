36635d27cdd657828910e4bb3309429216a4f173
mras2303 <marek.kowalonek@gmail.com>
2019-06-01 16:59:57 +0200

    123

--- src/chunk.test.js
+++ src/chunk.test.js
@@ -5,7 +5,7 @@ test('chunkArray function exists', () => {
 })
 
 test('chunk an array of 10values with length of 2', () => {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 9, 9, 10]
     const len = 2
     const chunkedArray = chunkArray(numbers, len)
     expect(chunkedArray).toStrictEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
